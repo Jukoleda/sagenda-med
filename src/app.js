9864262a4a0766c0eca37784d6639b8d605b113f
Jukoleda <mde_96@hotmail.com>
2021-02-25 23:12:32 -0300

    Se agregan librerias necesarias y archivos vacios que estructuran el proyecto.

--- src/app.js
+++ src/app.js
@@ -5,3 +5,11 @@ const mongoose = require('mongoose');
 
 
 const app = express();
+
+mongoose.connect('mongodb://sa_root:root@localhost:27017/super_agenda', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(db => console.log('base de datos conectada'))
+.catch(err => console.log(err));
+
+const indexRoutes = require('./routes/index');
+
+
