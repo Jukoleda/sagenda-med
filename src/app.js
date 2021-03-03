b96310cdb79dc04de4119326f471fcf2c59a0cee
Jukoleda <mde_96@hotmail.com>
2021-03-03 01:22:23 -0300

    archivos faltanes commit anterior

--- src/app.js
+++ src/app.js
@@ -5,11 +5,21 @@ const mongoose = require('mongoose');
 
 
 const app = express();
-
+app.set('port', 3000);
 mongoose.connect('mongodb://sa_root:root@localhost:27017/super_agenda', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(db => console.log('base de datos conectada'))
 .catch(err => console.log(err));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 const indexRoutes = require('./routes/index');
+
+app.use('/', indexRoutes);
+
+
+app.listen(app.get('port'), () => {
+    console.log(`server listening at port ${app.get('port')}`);
+});
 
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+    res.render('index');
+});
 
 
 module.exports = router;
